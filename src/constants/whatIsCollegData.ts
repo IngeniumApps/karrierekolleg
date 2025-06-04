c2f9b4a345aa47735071a5e3cf3a0160eb2f82d5
sa-bri-na <huber_sabrina@me.com>
2025-06-04 21:35:39 +0200

    update: add chemie woman

--- src/constants/whatIsCollegData.ts
+++ src/constants/whatIsCollegData.ts
@@ -43,7 +43,7 @@ export const whatIsCollegData: Slide[] = [
         kind: 'colleg',
         title: "Lorem i",
         description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
-        image: "woman.webp",
+        image: "woman-chemie.webp",
         alt: "Join Us image",
         classNames: {
             container: "bg-yellow-600",
